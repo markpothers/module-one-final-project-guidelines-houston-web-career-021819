44f59e417b2adc28ddf278d12fc12a651052640c
github username <github email address>
2019-03-06 09:19:03 -0600

    minor updates

--- app/models/Product.rb
+++ app/models/Product.rb
@@ -4,4 +4,13 @@ class Product < ActiveRecord::Base
   has_many :merchants, through: :deals
   has_many :cities, through: :deals
 
+
+  def self.services
+    all_services = self.all.map do |product|
+      product.service
+    end
+    all_services.uniq
+  end
+
+
 end 
